329f9231131b69343162ca8d89d91cc33a33da3e
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-05-19 16:22:41 -0400

    wrote custom authorizer and custom authenticator. working with rails backend

--- app/authenticators/jwt.js
+++ app/authenticators/jwt.js
@@ -1,3 +1,59 @@
-import JWT from 'ember-simple-auth-token/authenticators/jwt';
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authenticators/base';
+import config from '../config/environment';
 
-export default JWT.extend({});
+const { RSVP: { Promise }, $: { ajax }, run } = Ember;
+
+export default Base.extend({
+  tokenEndpoint: `${config.host}/knock/auth_token`,
+
+  restore(data) {
+    return new Promise((resolve, reject) => {
+      if (!Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  authenticate(creds) {
+    const { identification, password } = creds;
+
+    const data = JSON.stringify({
+      auth: {
+        email: identification,
+        password
+      }
+    });
+
+    const requestOptions = {
+      url: this.tokenEndpoint,
+      type: 'POST',
+      data,
+      contentType: 'application/json',
+      dataType: 'json'
+    };
+
+    return new Promise((resolve, reject) => {
+      ajax(requestOptions).then((response) => {
+        const { jwt } = response;
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          resolve({
+            token: jwt
+          });
+        });
+      }, (error) => {
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          reject(error);
+        });
+      });
+    });
+  },
+
+  invalidate(data) {
+    return Promise.resolve(data);
+  }
+});
